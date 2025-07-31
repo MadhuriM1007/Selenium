2cd0362a7097f49d4af897356df3f1f7f18857a5
DELL <DELL@DESKTOP-HQFB8MF>
2025-08-01 00:09:06 +0530

    777

--- src/test/java/testcasestestng/GitPush233new.java
+++ src/test/java/testcasestestng/GitPush233new.java
@@ -3,7 +3,7 @@ package testcasestestng;
 public class GitPush233new {
 
 	public static void main(String[] args) {
-		System.out.println("GIT Conflict 3");System.out.println("GIT Conflict 4");
+		System.out.println("GIT Conflict 3");System.out.println("GIT Conflict 4");System.out.println("GIT Conflic");
 
 	}
 
