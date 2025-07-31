db7a91c24ac92c6f566e437291b52e5c63405b86
DELL <DELL@DESKTOP-HQFB8MF>
2025-08-01 00:03:51 +0530

    com33

--- src/test/java/testcasestestng/GitConflict.java
+++ src/test/java/testcasestestng/GitConflict.java
@@ -1,10 +1,13 @@
 package testcasestestng;
 
+import org.apache.poi.util.SystemOutLogger;
+
 public class GitConflict {
 
 	public static void main(String[] args) {
 	
 		System.out.println("Git workspace 1");
+		System.err.println("1");
 	}
 
 }
